d7bcf4e609fe5535894479ec4ab0a66b74045d4d
黄滚-404.ms <git@404.ms>
2022-09-30 00:07:35 +0800

    --

---
+++ pages/index.tsx
@@ -0,0 +1,65 @@
+import Button from 'components/button'
+import PageHeader from 'components/pageheader'
+import Project from 'components/project'
+import Page from 'components/page'
+
+import meowImg from 'public/projects/meow.png'
+import unhtmlImg from 'public/projects/unhtml.png'
+import klipmonster from 'public/projects/klipmonster.png'
+import kuafuImg from 'public/projects/kuafu.png'
+
+
+const projects = [
+    {
+        title: 'unhtml ',
+        description: '截图美化、代码转图片、markdown转图片工具',
+        link: 'unhtml.com',
+        image: unhtmlImg
+    },
+    {
+        title: 'meow',
+        description: 'meow Markdown 编辑器',
+        linkText: 'meow markdown writer',
+        link: 'github.com/xurenlu/meow',
+        image: meowImg,
+    },
+    {
+        title: 'kuafu gateway',
+        description: '一个代码仅1000行但功能相当强大的web gateway',
+        linkText: 'kuafu gateway',
+        link: 'github.com/gotapi/kuafu',
+        image: kuafuImg,
+    },
+    {
+        title: 'klipMonster',
+        description: '粘贴板小工具集，如树形结构查看json',
+        linkText: 'klipMonster.frontend-box',
+        link: 'github.com/xurenlu',
+        github: 'github.com/xurenlu',
+        image: klipmonster,
+    },
+
+]
+
+export async function getStaticProps() {
+    // https://github.com/vercel/next.js/discussions/12124
+    return {
+        props: {
+            allPostsData: [],
+        },
+    }
+}
+
+const Home = (): JSX.Element => (
+    <Page>
+        <PageHeader title="你好，欢迎来黄滚的主页 ！" description="我是一名全栈开发工程师, 这是我的个人网站">
+<Button href="/about">了解更多</Button>
+    </PageHeader>
+    <h2>个人项目</h2>
+{projects.map(project => (
+    <Project key={project.title} {...project} />
+))}
+</Page>
+)
+
+export default Home
