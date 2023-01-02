e18316adeaeb63c28ba63e5b83115b1e9088b317
黄滚-404.ms <git@404.ms>
2023-01-02 11:00:30 +0800

    --

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,61 +5,67 @@ import Page from 'components/page'
 
 import meowImg from 'public/projects/meow.png'
 import unhtmlImg from 'public/projects/unhtml.png'
-import klipmonster from 'public/projects/klipmonster.png'
 import kuafuImg from 'public/projects/kuafu.png'
-
+import dev4IpImage from 'public/projects/ip4dev.png'
 
 const projects = [
-    {
-        title: 'unhtml ',
-        description: '截图美化、代码转图片、markdown转图片工具',
-        link: 'unhtml.com',
-        image: unhtmlImg
-    },
-    {
-        title: 'meow',
-        description: 'meow Markdown 编辑器',
-        linkText: 'meow markdown writer',
-        link: 'github.com/xurenlu/meow',
-        image: meowImg,
-    },
-    {
-        title: 'kuafu gateway',
-        description: '一个代码仅1000行但功能相当强大的web gateway',
-        linkText: 'kuafu gateway',
-        link: 'github.com/gotapi/kuafu',
-        image: kuafuImg,
-    },
-    {
-        title: 'klipMonster',
-        description: '粘贴板小工具集，如树形结构查看json',
-        linkText: 'klipMonster.frontend-box',
-        link: 'github.com/xurenlu',
-        github: 'github.com/xurenlu',
-        image: klipmonster,
-    },
-
+  {
+    title: 'unhtml',
+    description: '截图美化、代码转图片、markdown转图片工具',
+    link: 'unhtml.com',
+    image: unhtmlImg,
+  },
+  {
+    title: 'meow',
+    description: 'meow Markdown 编辑器',
+    linkText: 'meow markdown writer',
+    link: 'github.com/xurenlu/meow',
+    image: meowImg,
+  },
+  {
+    title: 'kuafu gateway',
+    description: '一个代码仅1000行但功能相当强大的web gateway',
+    linkText: 'kuafu gateway',
+    link: 'github.com/gotapi/kuafu',
+    image: kuafuImg,
+  },
+  {
+    title: 'ip4dev',
+    description: '一个ip地理信息数据库',
+    linkText: 'ip4dev',
+    link: 'ip4.dev',
+    github: 'github.com/gotapi/gmm',
+    image: dev4IpImage,
+  },
 ]
 
 export async function getStaticProps() {
-    // https://github.com/vercel/next.js/discussions/12124
-    return {
-        props: {
-            allPostsData: [],
-        },
-    }
+  // https://github.com/vercel/next.js/discussions/12124
+  return {
+    props: {
+      allPostsData: [],
+    },
+  }
 }
 
 const Home = (): JSX.Element => (
-    <Page>
-        <PageHeader title="Hey strangers. I am alex." description={<>I wrote articles about Vue/React,Golang,Java and some others. <br/>And daily life sometimes.</>}>
-<Button href="/about">for more</Button>
+  <Page>
+    <PageHeader
+      title="Hey strangers. I am alex."
+      description={
+        <>
+          I wrote articles about Vue/React,Golang,Java and some others. <br />
+          And daily life sometimes.
+        </>
+      }
+    >
+      <Button href="/about">for more</Button>
     </PageHeader>
     <h2>Open source projects</h2>
-{projects.map(project => (
-    <Project key={project.title} {...project} />
-))}
-</Page>
+    {projects.map(project => (
+      <Project key={project.title} {...project} />
+    ))}
+  </Page>
 )
 
 export default Home
